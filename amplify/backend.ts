5f9a0bf3a3e30faccb1984b97caa82c39bfc4bc9
korn-dev <kasikorntripforwork@gmail.com>
2025-05-17 11:34:45 +0700

    add lambda function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { myFirstFunction } from "./my-first-function/resource";
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
 
 defineBackend({
   auth,
   data,
+  myFirstFunction,
 });
